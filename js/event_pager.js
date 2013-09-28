75909c4e0761863054713a04910409b31bcc14ad
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-09-28 15:11:58 +1000

    Able to display events on front page block, even there is no upcoming events

--- js/event_pager.js
+++ js/event_pager.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function () {
   jQuery('.uom-events .part-events:not(:first)').hide();
 
   jQuery('.event-pager').jqPagination({
+		page_string   : 'Event {current_page} of {max_page}',
     max_page : jQuery('.uom-events .part-events').length,
     paged : function(page) {
       jQuery('.uom-events .part-events').hide();
