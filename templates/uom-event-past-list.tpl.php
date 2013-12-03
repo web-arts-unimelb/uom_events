1f8f009b5f219f4c54dfcee33b523d8549e105d9
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-12-04 09:43:20 +1100

    Fix event pager on uom-event-past-list.tpl.php

--- templates/uom-event-past-list.tpl.php
+++ templates/uom-event-past-list.tpl.php
@@ -73,7 +73,8 @@
 	<div class="event-pager" id="event-pager-bottom">
 		<a href="#" class="first" data-action="first">&laquo;</a>
 		<a href="#" class="previous" data-action="previous">&lsaquo;</a>
-		<input type="text" readonly="readonly" data-max-page="40" disabled="disabled"/>
+		<input type="text" readonly="readonly" data-max-page="40" style="display:none" />
+    <span class="current_page_display"></span>
 		<a href="#" class="next" data-action="next">&rsaquo;</a>
 		<a href="#" class="last" data-action="last">&raquo;</a>
 	</div>
